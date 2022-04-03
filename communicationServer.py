d9eb0d05acedf227af53db3cf5cc6254e3d12b7e
barshein <82324960+barshein@users.noreply.github.com>
2022-04-03 22:01:30 +0300

    Add files via upload

---
+++ communicationServer.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask import request
+
+comm = Flask(__name__)
+
+@comm.route('/')
+def my_form():
+    return render_template('some.html')
+    # return render_template('ss.html')
+
+@comm.route('/', methods=['POST'])
+def login():
+    username = request.form['username']
+    # password = request.form['password']
+    print(username)
+    # print(password)
+    return "hi"
+
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(comm, host="0.0.0.0", port=8000)
