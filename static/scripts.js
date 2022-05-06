56e91a664769ffba87df238801d0ffe65c526903
naamasharir <naamasharir3@gmail.com>
2022-05-06 16:41:18 +0300

    added a new response banner and a chart with fixed values

--- static/scripts.js
+++ static/scripts.js
@@ -89,6 +89,11 @@ window.onload = function() {
     document.getElementById("sendDataDiv").style.visibility = 'hidden';
 }
 
+function sendOnClick() {
+  document.getElementById("ResponseBanner").className = '';
+  loadBanner();
+}
+
 //I added event handler for the file upload control to access the files properties.
 document.addEventListener("DOMContentLoaded", init, false);
 
@@ -351,4 +356,38 @@ function attachSignin(element) {
       }, function(error) {
         alert(JSON.stringify(error, undefined, 2));
       });
+}
+
+function loadBanner() {
+  var xyValues = [
+    {x:50, y:7},
+    {x:60, y:8},
+    {x:70, y:8},
+    {x:80, y:9},
+    {x:90, y:9},
+    {x:100, y:9},
+    {x:110, y:10},
+    {x:120, y:11},
+    {x:130, y:14},
+    {x:140, y:14},
+    {x:150, y:15}
+  ];
+  
+  new Chart("myChart", {
+    type: "scatter",
+    data: {
+      datasets: [{
+        pointRadius: 4,
+        pointBackgroundColor: "rgb(0,0,255)",
+        data: xyValues
+      }]
+    },
+    options: {
+      legend: {display: false},
+      scales: {
+        xAxes: [{ticks: {min: 40, max:160}}],
+        yAxes: [{ticks: {min: 6, max:16}}],
+      }
+    }
+  });  
 }
