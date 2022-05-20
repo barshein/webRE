c3e12273db08256b585281076ea4b5105779f2fd
naamasharir <naamasharir3@gmail.com>
2022-05-20 20:45:09 +0300

    changed description chart to bar chart

--- static/scripts.js
+++ static/scripts.js
@@ -591,39 +591,30 @@ function loadDescriptionBanner(response) {
   document.getElementById("grammarDescriptionSuggestion").textContent = resp.grammar_model.replacement_description;
   generatedNumber = "The Grammar grade for the uploaded description is: "+response.responseText
   document.getElementById("generatedNumber").textContent = generatedNumber
-  loadDescriptionChart();
+  loadDescriptionGrades();
 }
 
-function loadDescriptionChart() {
-  var xyValues = [
-    {x:50, y:7},
-    {x:60, y:8},
-    {x:70, y:8},
-    {x:80, y:9},
-    {x:90, y:9},
-    {x:100, y:9},
-    {x:110, y:10},
-    {x:120, y:11},
-    {x:130, y:14},
-    {x:140, y:14},
-    {x:150, y:15}
-  ];
-  
-  new Chart("myChart", {
-    type: "scatter",
+function loadDescriptionGrades() {
+  var xValues = ["Grammar", "Positive semantic"];
+  var yValues = [resp.grammar_model.grade, 70];
+  grammarGradeColor = colorGradeDecider(resp.grammar_model.grade)
+  var barColors = [grammarGradeColor, "green"];
+
+  new Chart("chart_DescriptionGradesByModel", {
+    type: "bar",
     data: {
+      labels: xValues,
       datasets: [{
-        pointRadius: 4,
-        pointBackgroundColor: "rgb(0,0,255)",
-        data: xyValues
+        backgroundColor: barColors,
+        data: yValues
       }]
     },
     options: {
       legend: {display: false},
-      scales: {
-        xAxes: [{ticks: {min: 40, max:160}}],
-        yAxes: [{ticks: {min: 6, max:16}}],
+      title: {
+        display: true,
+        text: "Description grades by model"
       }
     }
-  });  
+  });
 }
