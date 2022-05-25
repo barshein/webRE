d4410f75e2e802844efdc7516730ee809609d6ff
Your Name <you@example.com>
2022-05-25 13:18:11 +0300

    db finish

--- mongodb.py
+++ mongodb.py
@@ -1,12 +1,9 @@
 import pymongo
+import json
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["realEstateDB"]
 
-data = mydb["data"]
-customer = mydb["customer"]
-
-# should print "data" and "customer"
 collist = mydb.list_collection_names()
 if "data" in collist:
   print("The collection 'data' exists.")
@@ -14,48 +11,61 @@ if "data" in collist:
 if "customer" in collist:
   print("The collection 'customer' exists.")
 
+data = mydb["data"]
+customer = mydb["customer"]
+
+# x = data.delete_many({})
+# print(x.deleted_count, " documents deleted.")
+# x = customer.delete_many({})
+# print(x.deleted_count, " customer deleted.")
+
 def isDBcontaionEmail(email):
+  print("check if email : " + str(email) +" is in db")
   query = {"email": email}
-  details = customer.find(query)
-  if details: # not null
-    return True
-  else:
+  info = list(customer.find(query))
+  if info.__len__() == 0:
     return False
+  else:
+    return True
 
 def addData(json):
   res = data.insert_one(json)
-  print(res)
+  print("is data saved - " + str(res.acknowledged))
 
 def addCustomer(json):
   res = customer.insert_one(json)
-  print(res)
-
-def getCustomerSessionsByEmail(email):
-  query = {"email": email}
-  return customer.find(query, {"name": 0, "email": 0, "password": 0, "sessionIds": 1})
+  print("is custumer saved - " + str(res.acknowledged))
 
 def getDataBySessionId(sessionId):
   query = { "sessionId": sessionId }
-  info = data.find(query, { "sessionId": 0 })
+  info = list(data.find(query, { "sessionId": 0 }))[0]
   description = info["description"]
   dbPhotos = info["photos"]
-  photosList = []
-  for photo in dbPhotos:
-    photosList.append(photo)
+  # photosList = []
+  # for photo in dbPhotos:
+  #   photosList.append(photo)
+  # return description, photosList
+  return description, dbPhotos
 
-  return description, photosList
+def getCustomerSessionsByEmail(email):
+  if not isDBcontaionEmail(email):
+    # the user is "google user"
+    addCustomer({ "email":email, "name":"", "userName":"","":"","password":"","sessionIds":{} })
+  sessions = list(customer.find({"email": email}))[0]["sessionIds"]
+  return sessions
 
 def updateCustumerSessionsByEmail(email, newSessionId, SessionResult):
-  sessionIds = getCustomerSessionsByEmail(email)
-  sessionIds[newSessionId] = SessionResult
   query = {"email": email}
-  newvalues = {"$set": {"sessionIds": sessionIds}}
+  sessionIds = getCustomerSessionsByEmail(email)
+  if isinstance(sessionIds, str):
+    sessionIds = json.loads(sessionIds)
+  sessionIds[newSessionId] = SessionResult
+  newvalues = {"$set": {"sessionIds": json.dumps(sessionIds)}}
   customer.update_one(query, newvalues)
-
 
 def couldLogin(email, password):
   query = {"email": email, "password": password}
-  info = data.find(query, {"sessionId": 0})
-  if info:
+  info = list(customer.find(query))
+  if info.__len__() == 0:
     return 0, ""
-  return 1, info["name"]
+  return 1, info[0]["name"]
